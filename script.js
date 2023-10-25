79c4b8109aed10fad2acb058e7a1405ba93610fd
Rodrigo <roddai@hotmail.com>
2023-10-25 12:46:40 -0300

    implementa as funções que criam os arrais dos selects e adiciona options aos selects

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const category = document.querySelector('#category');
 const categories = Object.keys(pets).sort();
 const selects = document.querySelectorAll('select');
+const arrayOfSomeKeys = ['name', 'breed', 'color', 'size', 'temperament'];
 
 // Percorre o array com as categorias de pets
 const loops = (array) => array.forEach((category) => addCategoryOptions('option', category));
@@ -13,8 +14,6 @@ const addCategoryOptions = (element, petCategory) => {
   optionElement.value = petCategory;
   
   category.appendChild(optionElement);
-
-  categoryClicked();
 }
 
 // Habilita ou desabilita selects
@@ -22,12 +21,42 @@ const categoryClicked = () => {
   category.addEventListener('change', () => {
     if (category.value !== 'category') {
       selects.forEach((select) => select.disabled = false);
+      addOptionsToSelects();
     } else {
       location.reload();
     }
   });
 }
 
+// Cria arrays sem repetição de valores
+const arrayData = (petCategory, key) => {
+  const array = [];
+
+  pets[petCategory].forEach((pet) => {
+    if (!array.includes(pet[key])) {
+      array.push(pet[key]);
+    }
+  })
+
+  return array.sort();
+}
+
+// Adiciona as opções nos demais selects
+const addOptionsToSelects = () => {
+  arrayOfSomeKeys.forEach((key) => {
+    const categorySelected = document.querySelector('#category').value;
+    const selectProperty = document.querySelector(`#${key}`);
+    const array = arrayData(categorySelected, key);
+    
+    array.forEach((feature) => {
+      const optionElement = document.createElement('option');
+      optionElement.innerText = feature;
+      selectProperty.appendChild(optionElement);
+    });
+  });
+}
+
 window.onload = () => {
   loops(categories);
+  categoryClicked();
 }
